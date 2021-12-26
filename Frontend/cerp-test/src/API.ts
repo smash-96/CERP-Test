7c344bea4398ee5e6d7aefbfdd9f9be54f1d82f8
smash-96 <maisamshah57@gmail.com>
2021-12-26 20:28:41 +0500

    misc deployment change

--- Frontend/cerp-test/src/API.ts
+++ Frontend/cerp-test/src/API.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosResponse } from "axios";
 
 // const baseUrl: string | undefined = process.env.REACT_APP_URL_PATH;
-const baseUrl: string | undefined = "https://todo-list-cerp.herokuapp.com";
+const baseUrl: string = "https://todo-list-cerp.herokuapp.com";
 
 export const getLists = async (): Promise<AxiosResponse<ListApiDataType>> => {
   try {
--- Frontend/cerp-test/src/index.tsx
+++ Frontend/cerp-test/src/index.tsx
@@ -5,8 +5,6 @@ import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
-import { BrowserRouter } from "react-router-dom";
-
 ReactDOM.render(
   <React.StrictMode>
       <App />
